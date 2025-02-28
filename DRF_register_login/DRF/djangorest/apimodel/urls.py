096c276c955e2bdccacb72be6367debfa1c6c3e6
Nikhilpn <nikhilpnnick1234@gmail.com>
2025-02-28 07:12:11 -1200

    register_login_logout

--- DRF_register_login/DRF/djangorest/apimodel/urls.py
+++ DRF_register_login/DRF/djangorest/apimodel/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('register/',Registerview.as_view()),
     path('login/',Loginview.as_view()),
     path('user/',UserView.as_view()),
+    path('logout/',LogoutView.as_view()),
 ]
