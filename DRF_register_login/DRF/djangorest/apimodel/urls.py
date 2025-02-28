e1e52772fec3faf6678f064dc1aa0cb3349c64aa
Nikhilpn <nikhilpnnick1234@gmail.com>
2025-02-27 12:45:57 -1200

    api_register_login_user

---
+++ DRF_register_login/DRF/djangorest/apimodel/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from restapp.views import *
+
+urlpatterns=[
+    path('register/',Registerview.as_view()),
+    path('login/',Loginview.as_view()),
+    path('user/',UserView.as_view()),
+]
