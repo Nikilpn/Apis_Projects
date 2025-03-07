0cf2e31405e21c78655236de33b4495d65bee973
Nikhilpn <nikhilpnnick1234@gmail.com>
2025-03-06 19:40:12 -1200

    Removed Drf_ViewSet and Drf_view_Set_class/songbox/api from repository

--- Drf_ViewSet/movieproject/restapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from restapp.models import Movie
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model =Movie
-        fields = '__all__'
-        read_only_fields = ['id']
